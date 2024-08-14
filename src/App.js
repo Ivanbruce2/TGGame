ff70058ea6ea03fa1675f5da3fec48c12784c2ea
Ivanbruce2 <Ivanbruce@hotmail.com>
2024-08-14 23:06:58 +0800

    s

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { retrieveLaunchParams } from '@telegram-apps/sdk';
 import './App.css';
-import WalletDisplay from './components/WalletDisplay/WalletDisplay';
 
 function App() {
   const [userID, setUserID] = useState('');
