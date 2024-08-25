bd4115744933b695d9b8229c5a70531269dd4e83
Admin <ivanbruce@hotmail.com>
2024-08-25 10:45:50 +0800

    m

--- src/App.js
+++ src/App.js
@@ -197,8 +197,7 @@ console.log(userID)
 console.log(gameStatus.player1ID)
 console.log("player choice"+gameStatus.player1Choice)
   if (gameStatus.status === 'waiting') {
-    console.log("here?")
-    if (userID === gameStatus.player1ID && (gameStatus.player1Choice === '' || gameStatus.player1Choice === null || gameStatus.player1Choice === undefined)) {
+    if (userID === gameStatus.player1ID) {
       console.log("Come gere?")
       return `You have ${countdown} seconds to make your move else you will be kicked out.`;
     }
